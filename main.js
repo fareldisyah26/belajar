124029c19ff367b73b14264ae9d48295a85256d4
fareldisyah26 <144892213+fareldisyah26@users.noreply.github.com>
2023-10-18 22:30:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,88 +1,94 @@
-// console.log("Hello World")
-// alert("Ada Notifikasi")
-// prompt("Pinjam duaratus")
+// console.log("Kiw Kiw")
+// alert("ada notifikasi")
+// prompt("Bismillah Sejuta")
 
-// var Promnet = "Coding is Easy"
+// var Promnet = "aura is a fighter"
 // console.log(Promnet)
-// var Promnet = "Coding is Hard"
-// console.log(Promnet)
-// var Promnet = "Nothing is Impossible"
+// var Promnet = "aura is a fighter"
 // console.log(Promnet)
 
-// let Promnet = "coding is fun"
 // console.log(Promnet)
-// let Promnet = " coding is fun"
+// var Promnet = "Shadow Kill"
 // console.log(Promnet)
 
-// const Promnet = " Coding is not my life"
-// console.log(Promnet);
-// const Promnet = " Coding is not my life banget"
-// console.log(Promnet);
+// let Promnet = "Coding Farel"
+// console.log(Promnet)
+// let Promnet = "Coding Farel"
+// console.log(Promnet)
 
-// let totalPoin = prompt("Masukkan Poin Anda")
-//     if(totalPoin > 100){
-//         document.write("Congratulation");
-//         }else{
-//             document.write("Keep Trying");
+// const Promnet = "Coding is fun"
+// console.log(Promnet)
+// const Promnet = "Coding not fun"
+// console.log(Promnet)
+
+// let totalPoin = prompt("Massukan Poin Anda")
+//     if(totalPoin >= 100){
+//         document.write("Keren");
+//     }else{
+//         document.write("Bismillah Sejuta");
 //     }
 
 // let x = 6;
 // let y = 3;
 
-// if(x<10 && y>1){
-//     document.write(a = "True");
+//     if(x < 10 && y > 1){
+//         document.write("True");
 //     }else{
-//         document.write(a = "False");
-// }
+//         doucument.write("False");
+//     }
+    
 
-// if(x<10 && y<1){
-//     document.write(b = "True");
-//     }else{
-//         document.write(b = "False");
-// }
+//     console.log(x < 10 && y > 1);
+//     console.log(x < 10 && y < 1);
+//     console.log (x == 5 || y==5);
+//     console.log(x==6 || y==5);
+//     console.log(! (x==y));
 
-// if(x==5||y==5) {
-//     document.write(c = "True");
-//     }else{
-//         document.write(c = "False");
-// }
+//     document.write(x < 10 && y > 1);
+//     document.write("<Br>");
+//     document.write(x < 10 && y < 1);
+//     document.write("<Br>");
+//     document.write(x == 5 || y==5);
+//     document.write("<Br>");
+//     document.write(x==6 || y==5);
+//     document.write("<Br>");
+//     document.write(! (x==y));
 
-// if(x==6||y==5) {
-//     document.write( d = "True");
-//     }else{
-//         document.write( d = "False");
-// }
+let p = document.querySelector("P");
+let button = document.querySelector("button");
+let input = document.querySelector("input");
 
-// if(!(x==y)) {
-//     document.write( e = "True");
-//     }else{
-//         document.write( e = "False");
-// }
+button.addEventListener('click', function () {
+    let isi = input.value;
+    console.log(isi);
 
-// let x = 6;
-// let y = 3;
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
-// console.log(x<10 && y>1);
-// console.log(x<10 && y<1);
-// console.log(x==5||y==5);
-// console.log(x==6||y==5);
-// console.log(!(x==y));
+    let label = document.createElement("label");
+    label.appendChild(checkbox);
+    label.appendChild(document.createTextNode(isi));
 
-// document.write(x<10 && y>1);
-// document.write("<br>");
-// document.write(x<10 && y<1);
-// document.write("<br>");
-// document.write(x==5||y==5);
-// document.write("<br>");
-// document.write(x==6||y==5);
-// document.write("<br>");
-// document.write(!(x==y));
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+  
+    label.appendChild(deleteButton);
 
-// let p = document.querySelector("p")
-// let button = document.querySelector("button")
-// let input = document.querySelector("input")
-// button.addEventListener('click', function(){
-//     let isi = input.value
-//     console.log(isi)
-//     p.innerHTML = isi
-// })
+    p.appendChild(label);
+
+    p.appendChild(document.createElement("br"));
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            label.style.textDecoration = 'line-through'; 
+        } else {
+            label.style.textDecoration = 'none'; 
+        }
+    });
+
+deleteButton.addEventListener('click', function () {
+        p.removeChild(label);
+    });
+
+    input.value = "";
+
+});
